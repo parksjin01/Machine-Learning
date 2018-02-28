aaf6b4e511d3b0237b11c22c3450cec9d47099b2
Damotorie <parksjin01@gmail.com>
2018-02-28 21:59:43 +0900

    Add normalization, logarithmatic in pre processing module

--- TF_example/Weather/Pre_processing.py
+++ TF_example/Weather/Pre_processing.py
@@ -1,6 +1,8 @@
 # -*- encoding:utf8 -*-
 import sys
 import random
+import math
+import matplotlib.pyplot as plt
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -12,7 +14,10 @@ class Preprocess():
         self.output = []
         files = os.listdir(path)
         with open('Header', 'r') as f:
-            head = unicode(f.read()).split(' ')
+            if path == './data/day':
+                head = unicode(f.read()).split('\n')[1].split(' ')
+            else:
+                head = unicode(f.read()).split('\n')[0].split(' ')
         header = [head.index(word) for word in header]
         for each_file in files:
             is_header = 0
@@ -41,7 +46,7 @@ class Preprocess():
                     for h_idx in header:
                         try:
                             if h_idx == 1:
-                                line_data.append(float(tmp_line[h_idx].split(':')))
+                                line_data.append(float(tmp_line[h_idx].split(' ')[1].split(':')[0]))
                             elif tmp_line[h_idx] == '':
                                 line_data.append(-1.0)
                             else:
@@ -54,6 +59,23 @@ class Preprocess():
         #     print t
         # print self.feature
 
+    def normalization(self):
+        mean = [0] * len(self.feature[0])
+        minimum = self.feature[0]
+        maximum = self.feature[1]
+        for line in self.feature:
+            mean = [mean[i]+line[i]/float(len(self.feature)) for i in range(len(line))]
+            for idx in range(len(line)):
+                if minimum[idx] > line[idx]:
+                    minimum[idx] = line[idx]
+                if maximum[idx] < line[idx]:
+                    maximum[idx] = line[idx]
+        min_max = [maximum[i] - minimum[i] for i in range(len(maximum))]
+        data = []
+        for line in self.feature:
+            data.append([(line[i]-mean[i])/min_max[i] for i in range(len(line))])
+        self.feature = data
+        
 
     def next_batch(self, batch_size):
         idx = random.sample(range(len(self.feature)), batch_size)
@@ -61,7 +83,28 @@ class Preprocess():
         batch_output = [self.output[m_idx] for m_idx in idx]
         return batch_feature, batch_output
 
+    def logarithmatic(self):
+        for idx1 in range(len(self.feature)):
+            for idx2 in range(len(self.feature[0])):
+                try:
+                    self.feature[idx1][idx2] = math.log(self.feature[idx1][idx2], 100)
+                except:
+                    self.feature[idx1][idx2] = 0
 
-data = Preprocess('./data', [u'기온', u'강수량', u'습도'])
-print data.next_batch(10)
-print random.sample(range(6), 3)
+    def visualization(self, idx):
+        data = []
+        for d in self.feature[0]:
+            data.append([d])
+        for line in self.feature[1:]:
+            for idx in range(len(line)):
+                data[idx].append(line[idx])
+            # data = [data[idx].append(d[idx]) for idx in range(len(d))]
+        plt.hist(data[idx], bins=20)
+        plt.show()
+
+# data = Preprocess('./data', [u'시간', u'기온', u'강수량', u'습도'])
+# data.logarithmatic()
+# data.normalization()
+# data.visualization(1)
+# print data.next_batch(10)
+# print random.sample(range(6), 3)
